17e87f7c2f7f6c0969a411cf9b00d446ed271c6e
bwatts64 <32721128+bwatts64@users.noreply.github.com>
2020-10-26 13:43:09 -0400

    Changed time to 7:30am

--- AzureCost_to_LogAnalytics/AzureCost_to_LogAnalytics/AzureCost_to_LogAnalytics.cs
+++ AzureCost_to_LogAnalytics/AzureCost_to_LogAnalytics/AzureCost_to_LogAnalytics.cs
@@ -92,7 +92,7 @@ namespace AzureCost_to_LogAnalytics
 
 
         [FunctionName("DailyCostLoad")]
-        public static async void Run([TimerTrigger("0 35 13 * * *")]TimerInfo myTimer, ILogger log)
+        public static async void Run([TimerTrigger("0 30 7 * * *")]TimerInfo myTimer, ILogger log)
         {
             DateTime start = DateTime.Now.AddDays(-1);
 
